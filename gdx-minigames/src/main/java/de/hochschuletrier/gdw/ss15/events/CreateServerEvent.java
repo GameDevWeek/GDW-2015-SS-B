a3935e969fb07ad83c818415b3ad9b8c93575fe5
Lusito <core@roughael.net>
2015-09-22 13:10:36 +0200

    netcode events

---
+++ gdx-minigames/src/main/java/de/hochschuletrier/gdw/ss15/events/CreateServerEvent.java
@@ -0,0 +1,32 @@
+package de.hochschuletrier.gdw.ss15.events;
+
+import com.badlogic.gdx.utils.SnapshotArray;
+
+public class CreateServerEvent {
+
+    public static interface Listener {
+        void onCreateServerEvent(int port, int maxPlayers, String userName);
+    }
+
+    private static final SnapshotArray<Listener> listeners = new SnapshotArray();
+
+    public static void emit(int port, int maxPlayers, String userName) {
+        Object[] items = listeners.begin();
+        for (int i = 0, n = listeners.size; i < n; i++) {
+            ((Listener)items[i]).onCreateServerEvent(port, maxPlayers, userName);
+        }
+        listeners.end();
+    }
+
+    public static void register(Listener listener) {
+        listeners.add(listener);
+    }
+
+    public static void unregister(Listener listener) {
+        listeners.removeValue(listener, true);
+    }
+
+    public static void unregisterAll() {
+        listeners.clear();
+    }
+}

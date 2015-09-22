beab0dd214e7cdd39b0ef2d9a92a2b94b346c0b6
Erik Antes <antesj@hochschule-trier.de>
2015-09-22 17:24:19 +0200

    Gamepad input ohne Sichtrichtung

--- gdx-minigames/src/main/java/de/hochschuletrier/gdw/ss15/game/input/InputGamePad.java
+++ gdx-minigames/src/main/java/de/hochschuletrier/gdw/ss15/game/input/InputGamePad.java
@@ -21,19 +21,30 @@ public class InputGamePad implements ControllerListener {
 
 	@Override
 	public boolean buttonDown(Controller controller, int buttonCode) {
-		System.out.println(buttonCode);
+        switch(buttonCode){
+            case 6:InputPuffer.pull = true; break;
+            case 7:InputPuffer.push = true; break;
+        }
 		return false;
 	}
 
 	@Override
 	public boolean buttonUp(Controller controller, int buttonCode) {
-		// TODO Auto-generated method stub
+		switch (buttonCode) {
+			case 6: InputPuffer.pull = false; break;
+		}
 		return false;
 	}
 
 	@Override
 	public boolean axisMoved(Controller controller, int axisCode, float value) {
-		System.out.println(axisCode + " : " + value);
+        if(value>0.2 || value <-0.2)
+        {
+            switch(axisCode){
+                case 0: InputPuffer.horizontal = 1.0f*value; break;
+                case 1: InputPuffer.vertical = 1.0f*value; break;
+            }   
+        }
 		return false;
 	}
 

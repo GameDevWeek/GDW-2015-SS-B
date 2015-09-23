56fd04b658f792ed5d27f98e2a62a33ff9fcf205
Lusito <core@roughael.net>
2015-09-23 21:41:03 +0200

    exported tilesets

---
+++ gdx-minigames/src/main/resources/data/maps/playgroundTileset.tsx
@@ -0,0 +1,55 @@
+<?xml version="1.0" encoding="UTF-8"?>
+<tileset name="playgroundTileset" tilewidth="128" tileheight="128">
+ <image source="playgroundTileset.png" width="512" height="384"/>
+ <terraintypes>
+  <terrain name="mist" tile="-1"/>
+  <terrain name="ground" tile="-1"/>
+ </terraintypes>
+ <tile id="0" terrain="0,0,0,1">
+  <properties>
+   <property name="solid" value="true"/>
+  </properties>
+ </tile>
+ <tile id="1" terrain="0,0,1,1">
+  <properties>
+   <property name="solid" value="true"/>
+  </properties>
+ </tile>
+ <tile id="2" terrain="0,0,1,0">
+  <properties>
+   <property name="solid" value="true"/>
+  </properties>
+ </tile>
+ <tile id="3" terrain="0,1,0,1">
+  <properties>
+   <property name="solid" value="true"/>
+  </properties>
+ </tile>
+ <tile id="4" terrain="1,1,1,1">
+  <properties>
+   <property name="solid" value="true"/>
+  </properties>
+ </tile>
+ <tile id="5" terrain="1,0,1,0"/>
+ <tile id="6" terrain="0,1,0,0">
+  <properties>
+   <property name="solid" value="true"/>
+  </properties>
+ </tile>
+ <tile id="7" terrain="1,1,0,0"/>
+ <tile id="8" terrain="1,0,0,0">
+  <properties>
+   <property name="solid" value="true"/>
+  </properties>
+ </tile>
+ <tile id="9">
+  <properties>
+   <property name="solid" value="true"/>
+  </properties>
+ </tile>
+ <tile id="10">
+  <properties>
+   <property name="solid" value="true"/>
+  </properties>
+ </tile>
+</tileset>

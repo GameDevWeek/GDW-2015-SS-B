303218d835db309f1aa7a3270e02cc183d92af4f
Lusito <core@roughael.net>
2015-09-23 19:16:47 +0200

    merging test games

--- gdx-minigames/src/main/java/de/hochschuletrier/gdw/ss15/game/TestGame.java
+++ gdx-minigames/src/main/java/de/hochschuletrier/gdw/ss15/game/TestGame.java
@@ -1,11 +1,13 @@
 package de.hochschuletrier.gdw.ss15.game;
 
-
-
 import com.badlogic.ashley.core.Entity;
+import com.badlogic.gdx.Gdx;
+import com.badlogic.gdx.Input;
 
 import de.hochschuletrier.gdw.commons.gdx.assets.AssetManagerX;
 import de.hochschuletrier.gdw.commons.gdx.physix.PhysixComponentAwareContactListener;
+import de.hochschuletrier.gdw.commons.netcode.simple.NetClientSimple;
+import de.hochschuletrier.gdw.commons.netcode.simple.NetServerSimple;
 import de.hochschuletrier.gdw.commons.tiled.LayerObject;
 import de.hochschuletrier.gdw.commons.tiled.TiledMap;
 import de.hochschuletrier.gdw.ss15.game.components.ImpactSoundComponent;
@@ -13,17 +15,35 @@ import de.hochschuletrier.gdw.ss15.game.components.LocalPlayerComponent;
 import de.hochschuletrier.gdw.ss15.game.components.TriggerComponent;
 import de.hochschuletrier.gdw.ss15.game.contactlisteners.ImpactSoundListener;
 import de.hochschuletrier.gdw.ss15.game.contactlisteners.TriggerListener;
+import de.hochschuletrier.gdw.ss15.game.data.GameType;
 import de.hochschuletrier.gdw.ss15.game.data.Team;
 import de.hochschuletrier.gdw.ss15.game.systems.InputBallSystem;
 import de.hochschuletrier.gdw.ss15.game.systems.LimitedSmoothCameraSystem;
 import de.hochschuletrier.gdw.ss15.game.systems.MapRenderSystem;
 import de.hochschuletrier.gdw.ss15.game.systems.MovementSystem;
+import de.hochschuletrier.gdw.ss15.game.systems.network.NetClientSendInputSystem;
+import de.hochschuletrier.gdw.ss15.game.systems.network.NetClientUpdateSystem;
+import de.hochschuletrier.gdw.ss15.game.systems.network.NetServerSendSystem;
+import de.hochschuletrier.gdw.ss15.game.systems.network.NetServerUpdateSystem;
 import de.hochschuletrier.gdw.ss15.game.utils.MapLoader;
 
 public class TestGame extends AbstractGame {
+    private final NetServerSimple netServer;
+    private final NetClientSimple netClient;
 
     private TiledMap map;
 
+    public TestGame() {
+        this(null, null);
+    }
+    
+    public TestGame(NetServerSimple netServer, NetClientSimple netClient) {
+        this.netServer = netServer;
+        this.netClient = netClient;
+        if(netClient != null)
+            factoryParam.allowPhysics = false;
+    }
+    
     private void initLoadMap() {
         map = loadMap("data/maps/DummyMap_mit_Entitytypes_Fix.tmx");
         engine.getSystem(LimitedSmoothCameraSystem.class).initMap(map);
@@ -40,15 +60,29 @@ public class TestGame extends AbstractGame {
 
         MapLoader.generateWorldFromTileMapX(engine, physixSystem, map);
 
-        /* TEST SPIELER ERSTELLEN */
-        Entity player = MapLoader.createEntity(engine, "player", 100, 100, Team.BLUE);
-        player.add(engine.createComponent(LocalPlayerComponent.class));
+        if(netClient == null) {
+            /* TEST SPIELER ERSTELLEN */
+            Entity player = MapLoader.createEntity(engine, "player", 100, 100, Team.BLUE);
+            player.add(engine.createComponent(LocalPlayerComponent.class));
+        }
         
         setupPhysixWorld();
-
+        
 		// Gdx.input.setInputProcessor(new InputKeyboard());
         //
         // Controllers.addListener(new InputGamePad());
+        if(netServer != null) {
+            netServer.setHandler(engine.getSystem(NetServerUpdateSystem.class));
+            netServer.setListener(engine.getSystem(NetServerUpdateSystem.class));
+        } else if(netClient != null) {
+            netClient.setHandler(engine.getSystem(NetClientUpdateSystem.class));
+            netClient.setListener(engine.getSystem(NetClientUpdateSystem.class));
+        }
+    }
+
+    @Override
+    public void start() {
+        super.start();
     }
 
     @Override
@@ -56,15 +90,20 @@ public class TestGame extends AbstractGame {
         super.addSystems();
         engine.addSystem(new InputBallSystem(0));
         engine.addSystem(new MovementSystem(1));
+        
+        if(netServer != null) {
+            engine.addSystem(new NetServerSendSystem(netServer));
+            engine.addSystem(new NetServerUpdateSystem(netServer, GameType.MAGNET_BALL, getMapName()));
+        } else if(netClient != null) {
+            engine.addSystem(new NetClientSendInputSystem(netClient));
+            engine.addSystem(new NetClientUpdateSystem(netClient));
+        }
     }
 
     @Override
-    protected void addContactListeners(
-            PhysixComponentAwareContactListener contactListener) {
-        contactListener.addListener(ImpactSoundComponent.class,
-                new ImpactSoundListener());
-        contactListener.addListener(TriggerComponent.class,
-                new TriggerListener());
+    protected void addContactListeners(PhysixComponentAwareContactListener contactListener) {
+        contactListener.addListener(ImpactSoundComponent.class, new ImpactSoundListener());
+        contactListener.addListener(TriggerComponent.class, new TriggerListener());
     }
 
     private void setupPhysixWorld() {
@@ -80,5 +119,23 @@ public class TestGame extends AbstractGame {
                     "Map konnte nicht geladen werden: ");
 
         }
+    }
+
+    @Override
+    public void update(float delta) {
+        super.update(delta);
+
+        if(Gdx.input.isKeyJustPressed(Input.Keys.SPACE)) {
+            Entity ball = MapLoader.createEntity(engine, "ball", 100, 100, Team.BLUE);
+        }
+    }
+    
+    @Override
+    public void dispose() {
+        super.dispose();
+        if(netClient != null)
+            netClient.disconnect();
+        else if(netServer != null)
+            netServer.disconnect();
     }
 }

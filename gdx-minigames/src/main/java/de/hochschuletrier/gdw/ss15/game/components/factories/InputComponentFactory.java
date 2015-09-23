25ce122122a205ce0aae568d86e1a0076d93674e
meiert <meiert@hochschule-trier.de>
2015-09-23 14:42:47 +0200

    import fix

--- gdx-minigames/src/main/java/de/hochschuletrier/gdw/ss15/game/components/factories/InputComponentFactory.java
+++ gdx-minigames/src/main/java/de/hochschuletrier/gdw/ss15/game/components/factories/InputComponentFactory.java
@@ -1,8 +1,6 @@
 package de.hochschuletrier.gdw.ss15.game.components.factories;
 
-import com.sun.tools.doclets.internal.toolkit.util.DocFinder;
 import de.hochschuletrier.gdw.ss15.game.components.InputBallComponent;
-import de.hochschuletrier.gdw.ss15.game.components.MovableComponent;
 
 /**
  * Created by isik on 23.09.15.

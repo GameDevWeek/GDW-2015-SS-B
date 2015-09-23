7bfb42ba15551a1e147f093c4118a639d05ed401
Özkan Isik <isiko@hochschule-trier.de>
2015-09-23 14:29:34 +0200

    Add MovementSystem (This time really)

--- gdx-minigames/src/main/java/de/hochschuletrier/gdw/ss15/game/components/factories/InputComponentFactory.java
+++ gdx-minigames/src/main/java/de/hochschuletrier/gdw/ss15/game/components/factories/InputComponentFactory.java
@@ -1,7 +1,15 @@
 package de.hochschuletrier.gdw.ss15.game.components.factories;
 
+import com.sun.tools.doclets.internal.toolkit.util.DocFinder;
+import de.hochschuletrier.gdw.ss15.game.components.InputBallComponent;
+import de.hochschuletrier.gdw.ss15.game.components.MovableComponent;
+
 /**
  * Created by isik on 23.09.15.
  */
-public class InputComponentFactory {
+public class InputComponentFactory extends SimpleComponentFactory {
+
+    public InputComponentFactory() {
+        super("Input", InputBallComponent.class);
+    }
 }

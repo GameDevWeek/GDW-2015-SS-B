e18f69a6d74c39ef9e920650a65a9019bfc76e7c
Lusito <core@roughael.net>
2015-09-24 10:05:39 +0200

    map fix

---
+++ gdx-minigames/src/main/resources/data/maps/playgroundTileset.tsx
@@ -0,0 +1,51 @@
+<?xml version="1.0" encoding="UTF-8"?>
+<tileset name="playgroundTileset" tilewidth="128" tileheight="128">
+ <image source="playgroundTileset.png" width="512" height="384"/>
+ <tile id="0">
+  <properties>
+   <property name="solid" value="true"/>
+  </properties>
+ </tile>
+ <tile id="1">
+  <properties>
+   <property name="solid" value="true"/>
+  </properties>
+ </tile>
+ <tile id="2">
+  <properties>
+   <property name="solid" value="true"/>
+  </properties>
+ </tile>
+ <tile id="3">
+  <properties>
+   <property name="solid" value="true"/>
+  </properties>
+ </tile>
+ <tile id="4">
+  <properties>
+   <property name="solid" value="true"/>
+  </properties>
+ </tile>
+ <tile id="5"/>
+ <tile id="6">
+  <properties>
+   <property name="solid" value="true"/>
+  </properties>
+ </tile>
+ <tile id="7"/>
+ <tile id="8">
+  <properties>
+   <property name="solid" value="true"/>
+  </properties>
+ </tile>
+ <tile id="9">
+  <properties>
+   <property name="solid" value="true"/>
+  </properties>
+ </tile>
+ <tile id="10">
+  <properties>
+   <property name="solid" value="true"/>
+  </properties>
+ </tile>
+</tileset>

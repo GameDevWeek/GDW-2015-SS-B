89cec5b85be6704baf7d5170a78361549c8eb2bd
Lusito <core@roughael.net>
2015-09-25 13:31:23 +0200

    resized tiles to 64x64

--- gdx-minigames/src/main/resources/data/maps/Dummy Gate.tsx
+++ gdx-minigames/src/main/resources/data/maps/Dummy Gate.tsx
@@ -1,4 +1,4 @@
 <?xml version="1.0" encoding="UTF-8"?>
-<tileset name="Dummy Gate" tilewidth="128" tileheight="128" tilecount="12">
- <image source="Dummy Gate.png" trans="ffffff" width="512" height="384"/>
+<tileset name="Dummy Gate" tilewidth="64" tileheight="64" tilecount="12">
+ <image source="Dummy Gate.png" trans="ffffff" width="256" height="192"/>
 </tileset>

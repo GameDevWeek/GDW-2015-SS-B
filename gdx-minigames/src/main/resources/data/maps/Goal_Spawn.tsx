d653e57a7a7737a71294376c5671ae3b3179785d
Lusito <core@roughael.net>
2015-09-25 22:27:50 +0200

    goal spawns tileset

--- gdx-minigames/src/main/resources/data/maps/Goal_Spawn.tsx
+++ gdx-minigames/src/main/resources/data/maps/Goal_Spawn.tsx
@@ -1,4 +1,16 @@
 <?xml version="1.0" encoding="UTF-8"?>
-<tileset name="Goal_Spawn" tilewidth="64" tileheight="64" tilecount="16">
- <image source="Goal_Spawn.png" width="256" height="256"/>
+<tileset name="Goal_Spawn" tilewidth="64" tileheight="64">
+ <image source="Goal_Spawn.png" width="320" height="192"/>
+ <tile id="0">
+  <properties>
+   <property name="Entitytype" value="PlayerSpawn"/>
+   <property name="Team" value="0"/>
+  </properties>
+ </tile>
+ <tile id="1">
+  <properties>
+   <property name="Entitytype" value="PlayerSpawn"/>
+   <property name="Team" value="1"/>
+  </properties>
+ </tile>
 </tileset>

37c9a7442bfb23d287dfb1e2df8780f8cae1ca7d
unknown <rftpool11@rft1102.rz.fh-trier.de>
2015-09-25 12:52:43 +0200

    added particles to the ball

--- gdx-minigames/src/main/java/de/hochschuletrier/gdw/ss15/game/ComponentMappers.java
+++ gdx-minigames/src/main/java/de/hochschuletrier/gdw/ss15/game/ComponentMappers.java
@@ -1,4 +1,4 @@
-package de.hochschuletrier.gdw.ss15.game;
+﻿package de.hochschuletrier.gdw.ss15.game;
 
 import com.badlogic.ashley.core.ComponentMapper;
 
@@ -12,6 +12,7 @@ import de.hochschuletrier.gdw.ss15.game.components.ImpactSoundComponent;
 import de.hochschuletrier.gdw.ss15.game.components.InputBallComponent;
 import de.hochschuletrier.gdw.ss15.game.components.MagneticFieldComponent;
 import de.hochschuletrier.gdw.ss15.game.components.MagneticInfluenceComponent;
+import de.hochschuletrier.gdw.ss15.game.components.BallParticlesComponent;
 import de.hochschuletrier.gdw.ss15.game.components.MovableComponent;
 import de.hochschuletrier.gdw.ss15.game.components.PlayerComponent;
 import de.hochschuletrier.gdw.ss15.game.components.PlayerSpawnComponent;
@@ -41,6 +42,8 @@ public class ComponentMappers {
     public static final ComponentMapper<PlayerComponent> player = ComponentMapper.getFor(PlayerComponent.class);
     public static final ComponentMapper<MagneticInfluenceComponent> magneticInfluence = ComponentMapper.getFor(MagneticInfluenceComponent.class);
     public static final ComponentMapper<MagneticFieldComponent> magneticField = ComponentMapper.getFor(MagneticFieldComponent.class);
+    public static final ComponentMapper<BallParticlesComponent> ballParticles = ComponentMapper.getFor(BallParticlesComponent.class);
+    
     public static final ComponentMapper<MovableComponent> movable = ComponentMapper.getFor(MovableComponent.class);
 
     /* Spawnpunkte für die Spieler */
---
+++ gdx-minigames/src/main/java/de/hochschuletrier/gdw/ss15/game/components/factories/MagnetComponentFactory.java
@@ -0,0 +1,29 @@
+package de.hochschuletrier.gdw.ss15.game.components.factories;
+
+import com.badlogic.ashley.core.Entity;
+
+import de.hochschuletrier.gdw.commons.gdx.ashley.ComponentFactory;
+import de.hochschuletrier.gdw.commons.utils.SafeProperties;
+import de.hochschuletrier.gdw.ss15.game.components.MagneticFieldComponent;
+
+public class MagnetComponentFactory extends SimpleComponentFactory{
+
+	public MagnetComponentFactory() {
+		super("Magnet",MagneticFieldComponent.class);
+		// TODO Auto-generated constructor stub
+	}
+
+//	@Override
+//	public String getType() {
+//		// TODO Auto-generated method stub
+//		return "magnet";
+//	}
+//
+//	@Override
+//	public void run(Entity entity, SafeProperties meta,
+//			SafeProperties properties, EntityFactoryParam param) {
+//		// TODO Auto-generated method stub
+//		
+//	}
+
+}

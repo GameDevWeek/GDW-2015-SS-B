fd7a4e44f122f26c7668f881bf62aa833f242dcb
Lusito <core@roughael.net>
2015-09-25 15:26:39 +0200

    moa mapfix

--- gdx-minigames/src/main/resources/data/maps/tileset_ground_SpawnsAndArea.tsx
+++ gdx-minigames/src/main/resources/data/maps/tileset_ground_SpawnsAndArea.tsx
@@ -1,5 +1,5 @@
 <?xml version="1.0" encoding="UTF-8"?>
-<tileset name="tileset_ground_SpawnsAndArea" tilewidth="64" tileheight="64" tilecount="12">
+<tileset name="tileset_ground_SpawnsAndArea" tilewidth="64" tileheight="64">
  <image source="tileset_ground_SpawnsAndArea.png" trans="ffffff" width="768" height="64"/>
  <tile id="3">
   <properties>
@@ -9,13 +9,13 @@
  </tile>
  <tile id="10">
   <properties>
-   <property name="Enitytype" value="BallSpawn"/>
+   <property name="Entitytype" value="BallSpawn"/>
    <property name="Team" value="0"/>
   </properties>
  </tile>
  <tile id="11">
   <properties>
-   <property name="Enitytype" value="BallSpawn"/>
+   <property name="Entitytype" value="BallSpawn"/>
    <property name="Team" value="1"/>
   </properties>
  </tile>

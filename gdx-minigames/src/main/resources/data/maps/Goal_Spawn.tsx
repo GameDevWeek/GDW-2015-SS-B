1ab164391ef0415d9b707b9c37858535c166be2d
Erik Antes <antesj@hochschule-trier.de>
2015-09-25 19:14:40 +0200

    goals and spawns

---
+++ gdx-minigames/src/main/resources/data/maps/Goal_Spawn.tsx
@@ -0,0 +1,4 @@
+<?xml version="1.0" encoding="UTF-8"?>
+<tileset name="Goal_Spawn" tilewidth="64" tileheight="64" tilecount="16">
+ <image source="Goal_Spawn.png" width="256" height="256"/>
+</tileset>

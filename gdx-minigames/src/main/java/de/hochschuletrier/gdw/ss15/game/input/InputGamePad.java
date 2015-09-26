7695b8c443e26b6f7d7837a56a59acc65c47c635
Lusito <core@roughael.net>
2015-09-27 00:19:49 +0200

    input fix & cleanup

--- gdx-minigames/src/main/java/de/hochschuletrier/gdw/ss15/game/input/InputGamePad.java
+++ gdx-minigames/src/main/java/de/hochschuletrier/gdw/ss15/game/input/InputGamePad.java
@@ -1,23 +1,9 @@
 package de.hochschuletrier.gdw.ss15.game.input;
 
 import com.badlogic.gdx.controllers.Controller;
-import com.badlogic.gdx.controllers.ControllerListener;
-import com.badlogic.gdx.controllers.PovDirection;
-import com.badlogic.gdx.math.Vector3;
+import com.badlogic.gdx.controllers.ControllerAdapter;
 
-public class InputGamePad implements ControllerListener {
-
-	@Override
-	public void connected(Controller controller) {
-		// TODO Auto-generated method stub
-		
-	}
-
-	@Override
-	public void disconnected(Controller controller) {
-		// TODO Auto-generated method stub
-		
-	}
+public class InputGamePad extends ControllerAdapter {
 
 	@Override
 	public boolean buttonDown(Controller controller, int buttonCode) {
@@ -47,29 +33,4 @@ public class InputGamePad implements ControllerListener {
         }
 		return false;
 	}
-
-	@Override
-	public boolean povMoved(Controller controller, int povCode, PovDirection value) {
-		// TODO Auto-generated method stub
-		return false;
-	}
-
-	@Override
-	public boolean xSliderMoved(Controller controller, int sliderCode, boolean value) {
-		// TODO Auto-generated method stub
-		return false;
-	}
-
-	@Override
-	public boolean ySliderMoved(Controller controller, int sliderCode, boolean value) {
-		// TODO Auto-generated method stub
-		return false;
-	}
-
-	@Override
-	public boolean accelerometerMoved(Controller controller, int accelerometerCode, Vector3 value) {
-		// TODO Auto-generated method stub
-		return false;
-	}
-
 }

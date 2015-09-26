2f113c5048f878238615f97c051998afe83a6b7d
Erik Antes <antesj@hochschule-trier.de>
2015-09-26 15:32:15 +0200

    no message

--- gdx-minigames/src/main/resources/data/maps/Goal_Spawn.tsx
+++ gdx-minigames/src/main/resources/data/maps/Goal_Spawn.tsx
@@ -1,5 +1,5 @@
 <?xml version="1.0" encoding="UTF-8"?>
-<tileset name="Goal_Spawn" tilewidth="64" tileheight="64">
+<tileset name="Goal_Spawn" tilewidth="64" tileheight="64" tilecount="15">
  <image source="Goal_Spawn.png" width="320" height="192"/>
  <tile id="0">
   <properties>
@@ -10,6 +10,30 @@
  <tile id="1">
   <properties>
    <property name="Entitytype" value="PlayerSpawn"/>
+   <property name="Team" value="1"/>
+  </properties>
+ </tile>
+ <tile id="3">
+  <properties>
+   <property name="Entitytype" value="goal_particles_bl"/>
+   <property name="Team" value="0"/>
+  </properties>
+ </tile>
+ <tile id="4">
+  <properties>
+   <property name="Entitytype" value="goal_particles_br"/>
+   <property name="Team" value="1"/>
+  </properties>
+ </tile>
+ <tile id="13">
+  <properties>
+   <property name="Entitytype" value="goal_particles_tl"/>
+   <property name="Team" value="0"/>
+  </properties>
+ </tile>
+ <tile id="14">
+  <properties>
+   <property name="Entitytype" value="goal_particles_tr"/>
    <property name="Team" value="1"/>
   </properties>
  </tile>
